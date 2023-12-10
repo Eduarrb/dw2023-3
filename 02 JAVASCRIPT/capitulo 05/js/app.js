c05378e6b2544cb7da3a903559e652dabca33bdb
Eduardo Arroyo <dj.shadowjeab@gmail.com>
2023-12-10 11:36:34 -0500

    js final

--- 02 JAVASCRIPT/capitulo 05/js/app.js
+++ 02 JAVASCRIPT/capitulo 05/js/app.js
@@ -1,5 +1,6 @@
 const artista = document.querySelector('.search');
 const btn = document.querySelector('.btn');
+const box = document.querySelector('.section__right__contenido__resultado__discografia');
 
 // EJECUTANDOSE SINCRONAMENTE
 // function saludar (){
@@ -10,13 +11,37 @@ const btn = document.querySelector('.btn');
 
 
 const obtenerJson = async function(artista){
-    const data = await fetch(`https://api.discogs.com/database/search?q=${artista}&type=master&artist=${artista}&format=album&token=HIjezRGadqgCAgYTcCOtPFyTVWlJSOoHyUhpmLhC`);
-    
-    const res = await data.json();
-    console.log(res)
+    if(artista === ''){
+        console.log('Debes ingresar un nombre de artista');
+    } else {
+        const data = await fetch(`https://api.discogs.com/database/search?q=${artista}&type=master&artist=${artista}&format=album&token=HIjezRGadqgCAgYTcCOtPFyTVWlJSOoHyUhpmLhC`);
+        
+        const res = await data.json();
+        console.log(res.results);
+        let plantilla = '';
+        res.results.forEach(el => {
+            // console.log(el.title);
+            // plantilla += `<h1>${el.title}</h1>`;
+            plantilla += `
+                <div class="section__right__contenido__resultado__discografia__item">
+                    <h3 class="section__right__contenido__resultado__discografia__item--titulo">${el.title}</h3>
+                    <div class="section__right__contenido__resultado__discografia__item__left">
+                        <img src="${el.cover_image}" alt="">
+                        <div class="section__right__contenido__resultado__discografia__item__left__info">
+                            <p>Año: <span>${el.year}</span></p>
+                            <p>País: <span>${el.country}</span></p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        })
+        // console.log(plantilla);
+        box.innerHTML = plantilla;
+    }
 }
 
 btn.addEventListener('click', function(){
     obtenerJson(artista.value);
+    artista.value = '';
 })
 
