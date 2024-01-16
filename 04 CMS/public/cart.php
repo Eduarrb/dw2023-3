c9218025889101d53fe4cdfdb9d16465d934077c
Eduardo <dj.shadowjeab@gmail.com>
2024-01-16 11:52:59 -0500

    cart inicio

---
+++ 04 CMS/public/cart.php
@@ -0,0 +1,126 @@
+<?php require_once("../resources/config.php"); ?>
+
+<?php include(VIEW_FRONT . DS . "head.php"); ?>
+    
+    <?php include(VIEW_FRONT . DS . "nav.php"); ?>
+
+    <section class="cart">
+        <div class="cart__container container">
+            <table class="cart__container__table">
+                <thead>
+                    <tr>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Total</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="img/mando.jpg" alt="">
+                        </td>
+                        <td>Mando</td>
+                        <td>S/ 369.90</td>
+                        <td>10</td>
+                        <td>S/ 3690.90</td>
+                        <td>
+                            <a href="#" class="btn btn-warning">
+                                <i class="fa-solid fa-minus"></i>
+                            </a>
+                            <a href="#" class="btn btn-success">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a href="#" class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/mando.jpg" alt="">
+                        </td>
+                        <td>Mando</td>
+                        <td>S/ 369.90</td>
+                        <td>10</td>
+                        <td>S/ 3690.90</td>
+                        <td>
+                            <a href="#" class="btn btn-warning">
+                                <i class="fa-solid fa-minus"></i>
+                            </a>
+                            <a href="#" class="btn btn-success">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a href="#" class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/mando.jpg" alt="">
+                        </td>
+                        <td>Mando</td>
+                        <td>S/ 369.90</td>
+                        <td>10</td>
+                        <td>S/ 3690.90</td>
+                        <td>
+                            <a href="#" class="btn btn-warning">
+                                <i class="fa-solid fa-minus"></i>
+                            </a>
+                            <a href="#" class="btn btn-success">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a href="#" class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/mando.jpg" alt="">
+                        </td>
+                        <td>Mando</td>
+                        <td>S/ 369.90</td>
+                        <td>10</td>
+                        <td>S/ 3690.90</td>
+                        <td>
+                            <a href="#" class="btn btn-warning">
+                                <i class="fa-solid fa-minus"></i>
+                            </a>
+                            <a href="#" class="btn btn-success">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a href="#" class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src="img/mando.jpg" alt="">
+                        </td>
+                        <td>Mando</td>
+                        <td>S/ 369.90</td>
+                        <td>10</td>
+                        <td>S/ 3690.90</td>
+                        <td>
+                            <a href="#" class="btn btn-warning">
+                                <i class="fa-solid fa-minus"></i>
+                            </a>
+                            <a href="#" class="btn btn-success">
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                            <a href="#" class="btn btn-danger">
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+    <?php include(VIEW_FRONT . DS . "footer.php"); ?>
